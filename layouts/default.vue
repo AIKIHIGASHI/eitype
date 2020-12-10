71faec9cda3d1d66c30beb224a59236fb1a59be5
AIKIHIGASHI <sheltie0805@gmail.com>
2020-12-10 21:20:58 +0900

    背景作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,14 @@
 <template>
-  <div>
+  <div class="container">
     <Nuxt />
+    <div class="container-bottom"></div>
   </div>
 </template>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -24,39 +18,28 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+body {
+  background-color: black;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.container {
+  background-image: url("~assets/黒板A.png");
+  height: 720px;
+  width: 1080px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  position: relative;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.container-bottom {
+  background-image: url("~assets/黒板B.png");
+  height: 45px;
+  width: 1080px;
+  background-color: pink;
+  position: absolute;
+  bottom: 0;
 }
 </style>
