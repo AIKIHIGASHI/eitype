323ef6552983dc14e0080ca33a5341d086e507f5
AIKIHIGASHI <sheltie0805@gmail.com>
2021-01-15 19:59:50 +0900

    ノートの中身作成

---
+++ components/InThePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="heading"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.heading {
+  border-bottom: 1px solid #93c5d8;
+  margin: 60px 20px 15px;
+  padding-bottom: 10px;
+}
+
+.row {
+  font-size: 13px;
+  border-bottom: 1px solid rgb(170, 170, 170);
+  margin: 0 25px;
+  height: 20px;
+  line-height: 20px;
+  text-align: left;
+}
+</style>
